b93cbb7a7c53779e1372c5f35ce5f08d00fb9eb1
tanya-giterman <tanya.giterman@gmail.com>
2024-04-08 19:28:59 -0400

    started new project

---
+++ src/main/java/com/academy/techcenture/utils/CommonUtils.java
@@ -0,0 +1,4 @@
+package com.academy.techcenture.utils;
+
+public class CommonUtils {
+}
---
+++ src/test/java/com/academy/techcenture/runner/RegressionRunner.java
@@ -0,0 +1,18 @@
+package com.academy.techcenture.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/java/com/academy/techcenture/features"},
+        glue = {"com/academy/techcenture/step_definitions", "com/academy/techcenture/hooks"},
+        dryRun = false,
+        tags = "@regression"
+
+)
+public class RegressionRunner {
+
+}
