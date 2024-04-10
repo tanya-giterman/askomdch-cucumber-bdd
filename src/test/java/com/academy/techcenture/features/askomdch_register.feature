1a7dd95cf97234f290c9132f26cf02e53964534f
varva <varvanskaaa@gmail.com>
2024-04-09 17:22:35 -0700

    add public void for register user steps

--- src/test/java/com/academy/techcenture/features/askomdch_register.feature
+++ src/test/java/com/academy/techcenture/features/askomdch_register.feature
@@ -7,9 +7,9 @@ Feature: AskOmDch User Registration Feature
     When user clicks on the account link
     Then user should be navigate to account page
     And user should be able to see register button
-    Then user adds fake username
-    And user adds fake email
-    And user adds fake password
+    Then user adds "Fake" username
+    And user adds "fake123@gmail.com" email
+    And user adds "Fake123" password
     When user clicks on register button
     Then user should see success registration message
     When user clicks on logout button
--- src/test/java/com/academy/techcenture/step_definitions/AskOmDchRegisterNewUserStepsDefinitions.java
+++ src/test/java/com/academy/techcenture/step_definitions/AskOmDchRegisterNewUserStepsDefinitions.java
@@ -1,48 +1,68 @@
 package com.academy.techcenture.step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static com.academy.techcenture.driver.Driver.getDriver;
 
 public class AskOmDchRegisterNewUserStepsDefinitions {
     @Then("user should be able to see register button")
     public void user_should_be_able_to_see_register_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user adds fake username")
-    public void user_adds_fake_username() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user adds fake email")
-    public void user_adds_fake_email() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user adds fake password")
-    public void user_adds_fake_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("user clicks on register button")
-    public void user_clicks_on_register_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user should see success registration message")
-    public void user_should_see_success_registration_message() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("user clicks on logout button")
-    public void user_clicks_on_logout_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user is on the login page")
-    public void user_is_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement registerBtn = getDriver().findElement(By.xpath("//*[@id=\"customer_login\"]/div[2]/h2"));
+        Assert.assertTrue(registerBtn.isEnabled());
+        registerBtn.click();
     }
 
+    @Then("user adds {string} username")
+    public void userAddsUsername(String username) {
+        WebElement usernameInput = getDriver().findElement(By.id("Fake"));
+        usernameInput.sendKeys(username);
+    }
+
+    @And("user adds {string} email")
+    public void userAddsEmail(String email) {
+        WebElement emailInput = getDriver().findElement(By.id("fake123@gmail.com"));
+        emailInput.sendKeys(email);
+    }
+
+    @And("user adds {string} password")
+    public void userAddsPassword(String password) {
+        WebElement passwordInput = getDriver().findElement(By.id("Fake123"));
+        passwordInput.sendKeys(password);
+    }
+
+    @When("user clicks on register button")
+    public void user_clicks_on_register_button() {
+        WebElement registerButton = getDriver().findElement(By.xpath("//*[@id=\"customer_login\"]/div[2]/h2"));
+        Assert.assertTrue(registerButton.isEnabled());
+        registerButton.click();
+
+    }
+
+    @Then("user should see success registration message")
+    public void user_should_see_success_registration_message() {
+        WebElement successMessage = getDriver().findElement(By.xpath("//*[@id=\"post-1235\"]/div/div[2]/div/div[2]/div/p[2]"));
+        Assert.assertTrue(successMessage.isDisplayed());
+    }
+
+    @When("user clicks on logout button")
+    public void user_clicks_on_logout_button() {
+        WebElement logoutBtn = getDriver().findElement(By.xpath("//*[@id=\"post-1235\"]/div/div[2]/div/div[2]/div/p[1]/a"));
+        Assert.assertTrue(logoutBtn.isEnabled());
+        logoutBtn.click();
+
+    }
+
+    @Then("user is on the login page")
+    public void user_is_on_the_login_page() {
+        String actualTitle = getDriver().getTitle();
+        Assert.assertEquals("Login page titles do not match", actualTitle, "askOmDch demo store. Login");
+        WebElement loginHeader = getDriver().findElement(By.className("page-title"));
+        Assert.assertTrue("Login Header is not displayed",loginHeader.isDisplayed());
+
+    }
 }
